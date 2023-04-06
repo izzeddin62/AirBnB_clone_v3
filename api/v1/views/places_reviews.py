6bce79087a4ee76b4f5f451a410a87a65dd8a993
izzeddin62 <i.ishimwe@alustudent.com>
2023-04-06 17:16:16 +0200

    get one review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -4,10 +4,11 @@
 place endpoint
 """
 from api.v1.views import app_views
-from flask import jsonify, abort, request
+from flask import jsonify, abort
 from models import storage
 from models.place import Place
-from models.city import City
+from models.review import Review
+
 
 @app_views.route('/places/<place_id>/reviews', methods=['GET'],
                     strict_slashes=False)
@@ -18,3 +19,12 @@ def get_all_reviews(place_id):
         abort(404)
     reviews = [review.to_dict() for review in place.reviews]
     return jsonify(reviews)
+
+
+@app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
+def get_review(review_id):
+    """get a review"""
+    review = storage.get(Review, review_id)
+    if review is None:
+        abort(404)
+    return jsonify(review.to_dict())
