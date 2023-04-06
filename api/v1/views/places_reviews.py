219f94f667c19c8933324013b230e3fd0678d4ef
izzeddin62 <i.ishimwe@alustudent.com>
2023-04-06 17:19:56 +0200

    post a new review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -3,8 +3,8 @@
 """
 place endpoint
 """
+from flask import jsonify, abort, request
 from api.v1.views import app_views
-from flask import jsonify, abort
 from models import storage
 from models.place import Place
 from models.review import Review
@@ -29,6 +29,7 @@ def get_review(review_id):
         abort(404)
     return jsonify(review.to_dict())
 
+
 @app_views.route('/reviews/<review_id>', methods=['DELETE'],
                     strict_slashes=False)
 def delete_review(review_id):
@@ -39,3 +40,22 @@ def delete_review(review_id):
     storage.delete(review)
     storage.save()
     return jsonify({}), 200
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['POST'],
+                    strict_slashes=False)
+def post_review(place_id):
+    """post a review"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    if not request.get_json():
+        abort(400, "Not a JSON")
+    if "user_id" not in request.get_json():
+        abort(400, "Missing user_id")
+    if "text" not in request.get_json():
+        abort(400, "Missing text")
+    review = Review(**request.get_json())
+    review.place_id = place_id
+    review.save()
+    return jsonify(review.to_dict()), 201
