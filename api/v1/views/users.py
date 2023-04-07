0aadc2a1c8e640fed7fb90d4959b28ce5f7caf5d
misskukuua <p,asare@alustudent.com>
2023-04-07 07:11:30 +0200

    PEP 8

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -5,12 +5,14 @@ from flask import jsonify, abort, request
 from models import storage
 from models.user import User
 
+
 @app_views.route('/users', strict_slashes=False)
 def get_users():
     """get users"""
     users = storage.all(User).values()
     users_list = [user.to_dict() for user in users]
     return jsonify(users_list)
+
 
 @app_views.route('/users/<user_id>', strict_slashes=False)
 def get_user(user_id):
