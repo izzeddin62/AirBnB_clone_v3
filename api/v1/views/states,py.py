31463b54a307427d0bd86729a9c6127722bea5dd
misskukuua <p.asare@alustudent.com>
2023-04-07 18:27:08 -0700

    api

--- api/v1/views/states,py.py
+++ api/v1/views/states,py.py
@@ -6,6 +6,7 @@ State objects that handles all default RESTFul API actions
 from api.v1.views import app_views
 from models import storage
 from models.state import State
+from flask import Flask, jsonify, abort, request
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -13,4 +14,10 @@ def get_states():
     states = storage.all(State).values()
     states_list = [state.to_dict() for state in states]
     return jsonify(states_list)
+
+
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=['GET'])
+def del_states(state_id):
+    """deletes a state object"""
+    if
 
